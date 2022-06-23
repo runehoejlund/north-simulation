fe158c5285f8adf2ac7223130fc8a3b452653d1c
runehoejlund <s173965@student.dtu.dk>
2022-06-23 14:59:07 +0200

    Run Bout on scratch disk. This breaks Docker paths

--- shared/NORTH/sbatch_north_full_ESEL.sh
+++ shared/NORTH/sbatch_north_full_ESEL.sh
@@ -3,12 +3,39 @@
 #SBATCH --partition=xeon40
 #SBATCH -N 1 # Number of nodes
 #SBATCH -n 40 # Total number of tasks
-#SBATCH --time=0-04:00:00
+#SBATCH --time=0-02:00:00
 #SBATCH --output=./slurm_out/north_full_%j_ESEL.log
 #SBATCH --error=./slurm_out/north_full_%j_ESEL_err.log
 
+# Set input and output directory
+IN_DIR=$PWD/data
+OUT_DIR=$PWD/data_$SLURM_JOB_ID
+mkdir -p $OUT_DIR
+
+# Load in modules
 module use ~/local/modules/modules/all
 module restore bout
 
+# Copy all necessary files to scratch folder
+cp $IN_DIR /scratch/$USER/data -r
+cp ./BoutFastOutput /scratch/$USER/BoutFastOutput -r
+cp ./dependencies /scratch/$USER/dependencies -r
+cp north_full_ESEL.cxx /scratch/$USER
+cp makefile /scratch/$USER
+echo "scratch dir content before cd"
+echo $(ls /scratch/$USER/ -a)
+cd /scratch/$USER
+
+echo "scratch dir content after cd"
+echo $(ls ./ -a)
+
+# Compile and run
 make
+# NOTE: Change IN_DIR if using restart and append=true
 mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false
+
+# When done: Move output files back to output directory
+mv /scratch/$USER/data/* $OUT_DIR
+
+# Clear cache
+rm -rf /scratch/$USER/*
