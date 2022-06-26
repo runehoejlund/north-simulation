3eecf5664ec426f289d6a1ffd3566125acb6775a
runehoejlund <s173965@student.dtu.dk>
2022-06-26 18:49:50 +0200

    renamed cylindrical -> toroidal

--- shared/NORTH/dependencies/utils/include/toroidalBCs.hxx
+++ shared/NORTH/dependencies/utils/include/toroidalBCs.hxx
@@ -1,16 +1,16 @@
-#ifndef __CYLINDRICALBCS_H__
-#define __CYLINDRICALBCS_H__
+#ifndef __TOROIDALBCS_H__
+#define __TOROIDALBCS_H__
 
 #include <bout.hxx>           // Includes all necessary classes and types
 #include <string>
 
 /*!
- * \class CylindricalBCs
+ * \class ToroidalBCs
  *
- * \brief Class which has member functions to set ghost points in cylindrical geometry
+ * \brief Class which has member functions to set ghost points in toroidal geometry
  *
  * Class for proper treatment of singularity
- * in cylindrical coordinates at center: rho = 0.
+ * in toroidal coordinates at center: r = 0.
  * The code is almost one-to-one taken from the
  * OwnBCs-class from the CELMA-project:
  * https://celma-project.github.io/
@@ -31,7 +31,7 @@
  *
  * \date 2022.19.06
  */
-class CylindricalBCs {
+class ToroidalBCs {
 private:
   // Data members
   int firstUpperYGhost; //!< Given that the processor has a boundary
@@ -39,24 +39,24 @@ private:
 
   // Member functions
   //! A function which loops over the inner boundary
-  void innerRhoCylinderLoop(Field3D &the_field, const int &y_start,
+  void applyCenterBCLoop(Field3D &the_field, const int &y_start,
                             const int &y_end);
   BoutReal x; //!< The value of x at the current index
   BoutReal z; //!< The value of z at the current index
 public:
   // Constructors
-  CylindricalBCs();
+  ToroidalBCs();
 
   // Destructors
   // Not needed as we have no dynamic memory allocation yet
 
   // Member functions
-  //! Specify ghost at inner \f$\rho\f$
-  void innerRhoCylinder(Field3D &the_field);
+  //! Specify ghost at inner \f$\r\f$
+  void applyCenterBC(Field3D &the_field);
 };
 
 // Function bodies of the non-inlined functions are located in the .cxx file
-#include "../src/cylindricalBCs.cxx"
+#include "../src/toroidalBCs.cxx"
 
 #endif
 
--- shared/NORTH/dependencies/utils/src/toroidalBCs.cxx
+++ shared/NORTH/dependencies/utils/src/toroidalBCs.cxx
@@ -1,7 +1,7 @@
-#ifndef __CYLINDRICALBCS_CXX__
-#define __CYLINDRICALBCS_CXX__
+#ifndef __TOROIDALBCS_CXX__
+#define __TOROIDALBCS_CXX__
 
-#include "../include/cylindricalBCs.hxx"
+#include "../include/toroidalBCs.hxx"
 #include <string>
 
 /*!
@@ -13,7 +13,7 @@
  *          should only be used for the processors using the respective
  *          boundaries
  */
-CylindricalBCs::CylindricalBCs() {
+ToroidalBCs::ToroidalBCs() {
   // yend = index value of last inner point on this processor
   firstUpperYGhost = mesh->yend + 1;
   // ystart = index value of first inner point on this processor
@@ -22,14 +22,14 @@ CylindricalBCs::CylindricalBCs() {
 
 // Member functions
 /*!
- * Sets the inner x ghost points of a Field3D in cylindrical coordinates.
+ * Sets the inner x ghost points of a Field3D in toroidal coordinates.
  *
  * \param[in] f The original field
  * \param[out] f The field after setting the inner boundary
  *
  * ## Idea:
  * We assume continuity for diametrically opposite points on the boundary
- * at rho = 0.
+ * at r = 0.
  * The innermost ghost points in x (x_index = 0), closest to the boundary)
  * will be set to the value of the innermost internal point (meaning all
  * points excluding the ghost points) which lies \f$\theta + \pi\f$ away
@@ -45,30 +45,30 @@ CylindricalBCs::CylindricalBCs() {
  * corresponding_zIndex = (current_ghost_zIndex + mesh->LocalNz/2) % (mesh->LocalNz)
  * ~~~
  *
- * where `x` is the radius, \f$\rho\f$, and z is azimuthal angle, \f$\theta\f$.
+ * where `x` is the radius, \f$\r\f$, and z is azimuthal angle, \f$\theta\f$.
  *
- * \sa innerRhoCylinderLoop
+ * \sa applyCenterBCLoop
  *
  * \note We are also setting the inner x-y corner points
  */
-void CylindricalBCs::innerRhoCylinder(Field3D &f) {
+void ToroidalBCs::applyCenterBC(Field3D &f) {
   if (mesh->firstX()) {
     // Set the boundary for the inner y points
-    innerRhoCylinderLoop(f, mesh->ystart, mesh->yend);
+    applyCenterBCLoop(f, mesh->ystart, mesh->yend);
     // Do the same for the ghost points in y
     if (mesh->firstY()) {
-      innerRhoCylinderLoop(f, 0, firstLowerYGhost);
+      applyCenterBCLoop(f, 0, firstLowerYGhost);
     }
     if (mesh->lastY()) {
       // Note that LocalNy starts counting from 1
-      innerRhoCylinderLoop(f, firstUpperYGhost, mesh->LocalNy - 1);
+      applyCenterBCLoop(f, firstUpperYGhost, mesh->LocalNy - 1);
     }
   }
 }
 
 // Auxiliary
 /*!
- * The actual loops which sets the ghost points for inner \f$\rho\f$
+ * The actual loops which sets the ghost points for inner \f$\r\f$
  *
  * \param[in] f The field to loop over
  * \param[in] yStart The start (inclusive) of the y index we are looping over
@@ -76,9 +76,9 @@ void CylindricalBCs::innerRhoCylinder(Field3D &f) {
  *
  * \param[out] f The field which has the inner boundaries set
  *
- * \sa innerRhoCylinder
+ * \sa applyCenterBC
  */
-void CylindricalBCs::innerRhoCylinderLoop(Field3D &f, const int &yStart,
+void ToroidalBCs::applyCenterBCLoop(Field3D &f, const int &yStart,
                                   const int &yEnd) {
   // Loop through all X and Y and all Z-points on the processor
   for (int xInd = 0; xInd < mesh->xstart; xInd++) {
