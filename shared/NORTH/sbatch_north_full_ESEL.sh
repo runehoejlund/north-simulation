e86e51b3bb1e097f90e6b850a6a2349051238d4e
Asbjørn Clod Pedersen <s173956@slid.fysik.dtu.dk>
2022-06-26 19:04:30 +0200

    New density source

--- shared/NORTH/sbatch_north_full_ESEL.sh
+++ shared/NORTH/sbatch_north_full_ESEL.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=xeon40
 #SBATCH -N 1 # Number of nodes
 #SBATCH -n 40 # Total number of tasks
-#SBATCH --time=0-02:00:00
+#SBATCH --time=0-01:05:00
 #SBATCH --output=./slurm_out/north_full_%j_ESEL.log
 #SBATCH --error=./slurm_out/north_full_%j_ESEL_err.log
 
@@ -26,9 +26,9 @@ cd /scratch/$USER
 
 # Compile and run
 make
-mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false
+mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 1
 # NOTE: Change IN_DIR if using restart and append=true
-# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true restart append=true
+# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true restart append=true wall_limit = 1
 # NOTE: Use this to set flags for input parameters while running
 # mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false laplace:flags=0
 
