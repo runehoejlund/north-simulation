3e759dc8cceb0cd64f67b5b4e006d32201d9f140
runehoejlund <s173965@student.dtu.dk>
2022-09-12 10:54:47 +0200

    debugging and module purge in sbatch-file

--- shared/NORTH/sbatch_north_full_ESEL.sh
+++ shared/NORTH/sbatch_north_full_ESEL.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=xeon40
 #SBATCH -N 1 # Number of nodes
 #SBATCH -n 40 # Total number of tasks
-#SBATCH --time=0-02:10:00
+#SBATCH --time=0-01:10:00
 #SBATCH --output=./slurm_out/north_full_%j_ESEL.log
 #SBATCH --error=./slurm_out/north_full_%j_ESEL_err.log
 
@@ -11,9 +11,13 @@
 IN_DIR=$PWD/data
 OUT_DIR=$PWD/data_$SLURM_JOB_ID
 
+echo $IN_DIR
+
 # Load in modules
+module purge
 module use ~/local/modules/modules/all
 module restore bout
+echo "Restored bout"
 
 # Copy all necessary files to scratch folder
 cp $IN_DIR /scratch/$USER/data -r
@@ -23,13 +27,19 @@ cp north_full_ESEL.cxx /scratch/$USER
 cp makefile /scratch/$USER
 cd /scratch/$USER
 
+echo | ls -a
+
 # Compile and run
 make
-mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 2
-# NOTE: Change IN_DIR if using restart and append=true
-#mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true restart append=true wall_limit = 2
-# NOTE: Use this to set flags for input parameters while running
-# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 1 laplace:flags=0
+
+echo "made that file"
+mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 1
+# # NOTE: Change IN_DIR if using restart and append=true
+# #mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true restart append=true wall_limit = 2
+# # NOTE: Use this to set flags for input parameters while running
+# # mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 1 laplace:flags=0
+
+echo "Done. Moving output files"
 
 # When done: Move output files back to output directory
 mkdir -p $OUT_DIR
