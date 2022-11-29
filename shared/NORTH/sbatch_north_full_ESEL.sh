d7c9529dda5e4ccb919ea5251ffc6f21b7addfa4
runehoejlund <s173965@student.dtu.dk>
2022-11-29 11:31:50 +0100

    Update scratch directory in sbatch script

--- shared/NORTH/sbatch_north_full_ESEL.sh
+++ shared/NORTH/sbatch_north_full_ESEL.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=vort_10
+#SBATCH --job-name=vort3
 #SBATCH --mail-type=NONE
 #SBATCH --partition=xeon24
 #SBATCH -N 1 # Number of nodes
 #SBATCH -n 24 # Total number of tasks
-#SBATCH --time=0-08:10:00
+#SBATCH --time=0-24:10:00
 #SBATCH --output=./slurm_out/north_full_%j_ESEL.log
 #SBATCH --error=./slurm_out/north_full_%j_ESEL_err.log
 
 # Set input and output directory
-IN_DIR=$PWD/data
+# IN_DIR=$PWD/data
 # NOTE: Change IN_DIR if using restart and append=true
-# IN_DIR=$PWD/data_5347831
+IN_DIR=$PWD/data_5674003
 OUT_DIR=$PWD/data_$SLURM_JOB_ID
+SCRATCH_DIR=/scratch/$USER/$SLURM_JOB_ID
 
 # Load in modules
 module purge
@@ -26,25 +27,26 @@ module restore bout
 # cd ../NORTH/
 
 # Copy all necessary files to scratch folder
-cp $IN_DIR /scratch/$USER/data -r
-cp ./BoutFastOutput /scratch/$USER/BoutFastOutput -r
-cp ./dependencies /scratch/$USER/dependencies -r
-cp north_full_ESEL.cxx /scratch/$USER
-cp makefile /scratch/$USER
-cd /scratch/$USER
+mkdir -p $SCRATCH_DIR
+cp $IN_DIR $SCRATCH_DIR/data -r
+cp ./BoutFastOutput $SCRATCH_DIR/BoutFastOutput -r
+cp ./dependencies $SCRATCH_DIR/dependencies -r
+cp north_full_ESEL.cxx $SCRATCH_DIR
+cp makefile $SCRATCH_DIR
+cd $SCRATCH_DIR
 
 # Compile and run
 make
 
-# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 2
+# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 8
 # NOTE: Change IN_DIR if using restart and append=true
-# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true restart append=true wall_limit = 2
+mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true restart append=true wall_limit = 24
 # NOTE: Use this to set flags for input parameters while running
-mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 8 north:vort_sink_strength=10
+# mpirun -n $SLURM_NTASKS ./north_full_ESEL stopCheck=true append=false wall_limit = 8 north:vort_sink_strength=10
 
 # When done: Move output files back to output directory
 mkdir -p $OUT_DIR
-mv /scratch/$USER/data/* $OUT_DIR
+mv $SCRATCH_DIR/data/* $OUT_DIR
 
 # Clear cache
-rm -rf /scratch/$USER/*
+rm -rf $SCRATCH_DIR/*
